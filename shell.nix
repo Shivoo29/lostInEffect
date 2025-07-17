6b8bbbcc247616574bcc1e336e5be6675d09c2ef
badmos-coder <badmos3322@gmail.com>
2025-07-18 05:24:21 +0530

    Add shell.nix for NixOS development environment

--- shell.nix
+++ shell.nix
@@ -1,12 +1,47 @@
-# shell.nix
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.git
-    pkgs.git-lfs
-    pkgs.python311
-    pkgs.python311Packages.numpy
-    pkgs.python311Packages.opencv4
+  buildInputs = with pkgs; [
+    python311
+    python3
+    python3Packages.numpy
+    (python311.withPackages (pyPkgs: with pyPkgs; [
+      numpy
+      opencv
+      scikit-learn
+      joblib
+      psutil
+      # Add other Python packages from your requirements.txt here if they are not already provided by Nixpkgs
+      # For example:
+      # librosa
+      # pandas
+      # matplotlib
+      # pillow
+      # jupyter
+      # notebook
+      # tqdm
+      # seaborn
+      # pyserial
+      # requests
+      # av
+      # Flask
+      # Flask_Toastr
+      # gitpython
+      # ipython
+      # kornia
+      # pims
+      # PyYAML
+      # scipy
+      # torch
+      # torchvision
+      # tensorboard
+      # Werkzeug
+    ]))
   ];
+
+  # Set environment variables if needed
+  shellHook = ''
+    echo "Entering Nix-managed development environment"
+    # You can add any commands here that should run when entering the shell
+  '';
 }
